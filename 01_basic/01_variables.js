dd1b58e8e457ed9fa0c19c1a1f9e405c9caf9ac6
Brajesh Das <brajeshdas117@gmail.com>
2026-04-27 15:07:24 +0000

    Learned about the Constant and Variables.

---
+++ 01_basic/01_variables.js
@@ -0,0 +1,9 @@
+const accountId = 1236546
+let accountEmail = "brajeshdas117@gmail.com"
+var accountPassword = "21134"
+accountCity = "Bhubaneswar"
+
+// accountId = 12345
+// console.log(accountId)
+
+console.table([accountId,accountEmail,accountPassword,accountCity])
