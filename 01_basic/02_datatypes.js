02b2721aff14e7d313f73dbdb87d737dd3a32bb3
Brajesh Das <brajeshdas117@gmail.com>
2026-04-30 10:03:52 +0000

    Datatypes

---
+++ 01_basic/02_datatypes.js
@@ -0,0 +1,24 @@
+"use strict"; // treat all JS code as newer version
+
+// alert (3 + 3) // We are using nodeJs, not browser
+
+// Data Types 
+
+let name = "Brajesh"      // String
+let age = 23              // Number
+let isLoggedIn = false    // Boolean
+
+console.log(typeof "Brajesh");
+
+// number => range: 2 to power 54
+// BigInt => large numbers 
+// String => ""
+// Boolean => true/false 
+// null => standalone value 
+// undefined => 
+// symbol => unique
+
+// Object
+
+console.log(typeof undefined); // undefined
+console.log(typeof null); // object
